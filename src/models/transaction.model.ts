78527e20d6e2062efa0fb651725d6d585cd9d679
Mekam Steve <stevemekam01@gmail.com>
2025-09-10 10:34:47 +0100

    feat(transaction): initialize the transaction model

--- src/models/transaction.model.ts
+++ src/models/transaction.model.ts
@@ -1,1 +1,14 @@
-export interface TransactionModel{}
+export interface TransactionModel {
+    id: string;
+    userId: string;
+    type: "credit" | "debit";
+    amount: number;
+    currency: string;
+    status: "pending" | "success" | "failed" | "cancelled";
+    method?: "card" | "mobile_money" | "bank_transfer" | "cash";
+    description?: string;
+    reference?: string;
+    createdAt: Date;
+    updatedAt?: Date;
+    metadata?: Record<string, any>;
+}
